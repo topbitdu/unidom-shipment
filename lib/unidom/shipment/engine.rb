16f334e3be5798e5b278b31810c2023cd3c8bd1e
Topbit Du <topbit.du@gmail.com>
2017-01-01 10:59:56 +0800

    1, Improve the Engine class to include the Engine Extension.

--- lib/unidom/shipment/engine.rb
+++ lib/unidom/shipment/engine.rb
@@ -1,19 +1,13 @@
-require 'unidom/common/yaml_helper'
-
 module Unidom
   module Shipment
 
     class Engine < ::Rails::Engine
 
+      include Unidom::Common::EngineExtension
+
       isolate_namespace ::Unidom::Shipment
 
-      initializer :load_config_initializers do |app|
-        Unidom::Common::YamlHelper.load_enum config: app.config, root: config.root
-      end
-
-      initializer :append_migrations do |app|
-        config.paths['db/migrate'].expanded.each { |expanded_path| app.config.paths['db/migrate'] << expanded_path } unless app.root.to_s.match root.to_s
-      end
+      enable_initializer enum_enabled: true, migration_enabled: true
 
     end
 
