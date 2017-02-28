421982ea4b894d257f39cc4387e2798a52fc51cf
Topbit Du <topbit.du@gmail.com>
2017-02-28 18:28:23 +0800

    1, Improve the Shipment Receipt spec for the validations on the #accepted_quantity attribute, & the #rejected_quantity attribute.

--- lib/rspec/models/unidom/shipment/shipment_receipt_spec.rb
+++ lib/rspec/models/unidom/shipment/shipment_receipt_spec.rb
@@ -22,6 +22,50 @@ describe Unidom::Shipment::ShipmentReceipt, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates', model_attributes, :accepted_quantity,
+      {                                       } => 0,
+      { accepted_quantity: nil                } => 2,
+      { accepted_quantity: ''                 } => 2,
+      { accepted_quantity: '1'                } => 0,
+      { accepted_quantity: 1                  } => 0,
+      { accepted_quantity: 'A'                } => 1,
+      { accepted_quantity: '1.23'             } => 0,
+      { accepted_quantity: 1.23               } => 0,
+      { accepted_quantity: '-0.01'            } => 1,
+      { accepted_quantity: -0.01              } => 1,
+      { accepted_quantity: '0'                } => 0,
+      { accepted_quantity: 0                  } => 0,
+      { accepted_quantity: '0.01'             } => 0,
+      { accepted_quantity: 0.01               } => 0,
+      { accepted_quantity: '999_999_999.99'   } => 0,
+      { accepted_quantity: 999_999_999.99     } => 0,
+      { accepted_quantity: '1_000_000_000'    } => 0,
+      { accepted_quantity: 1_000_000_000      } => 0,
+      { accepted_quantity: '1_000_000_000.01' } => 1,
+      { accepted_quantity: 1_000_000_000.01   } => 1
+
+    it_behaves_like 'validates', model_attributes, :rejected_quantity,
+      {                                       } => 0,
+      { rejected_quantity: nil                } => 2,
+      { rejected_quantity: ''                 } => 2,
+      { rejected_quantity: '1'                } => 0,
+      { rejected_quantity: 1                  } => 0,
+      { rejected_quantity: 'A'                } => 1,
+      { rejected_quantity: '1.23'             } => 0,
+      { rejected_quantity: 1.23               } => 0,
+      { rejected_quantity: '-0.01'            } => 1,
+      { rejected_quantity: -0.01              } => 1,
+      { rejected_quantity: '0'                } => 0,
+      { rejected_quantity: 0                  } => 0,
+      { rejected_quantity: '0.01'             } => 0,
+      { rejected_quantity: 0.01               } => 0,
+      { rejected_quantity: '999_999_999.99'   } => 0,
+      { rejected_quantity: 999_999_999.99     } => 0,
+      { rejected_quantity: '1_000_000_000'    } => 0,
+      { rejected_quantity: 1_000_000_000      } => 0,
+      { rejected_quantity: '1_000_000_000.01' } => 1,
+      { rejected_quantity: 1_000_000_000.01   } => 1
+
   end
 
 end
