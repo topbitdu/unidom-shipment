ef80488a852111573a6958958b77a388e6050284
Topbit Du <topbit.du@gmail.com>
2017-03-13 09:19:39 +0800

    1, Improve the Shipment Package spec for the ``has_many :items, class_name: 'Unidom::Shipment::ShipmentPackageItem', foreign_key: :package_id`` macro.

--- lib/rspec/models/unidom/shipment/shipment_package_spec.rb
+++ lib/rspec/models/unidom/shipment/shipment_package_spec.rb
@@ -14,6 +14,18 @@ describe Unidom::Shipment::ShipmentPackage, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    shipment_package_item_1_attributes = {
+      shipment_item_id: SecureRandom.uuid,
+      quantity:         10.00
+    }
+
+    shipment_package_item_2_attributes = {
+      shipment_item_id: SecureRandom.uuid,
+      quantity:         20.00
+    }
+
+    it_behaves_like 'has_many', model_attributes, :items, Unidom::Shipment::ShipmentPackageItem, [ shipment_package_item_1_attributes, shipment_package_item_2_attributes ]
+
   end
 
 end
